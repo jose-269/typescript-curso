b2663fee567282074785d8be600d9588ba7b26b5
jose-269 <jose.luis@destacados.cl>
2022-02-01 13:57:43 -0300

    terminado seccion clases hasta constructores privados

--- classes/private-constructor.ts
+++ classes/private-constructor.ts
@@ -1,6 +1,36 @@
 (() => {
+  class Apocalipsis {
+
+    static instance: Apocalipsis;
+
+    private constructor( // contructor privado solo se puede llamar dentro de la clase
+      public name: string,
+    ) {};
+
+    static callApocalipsis(): Apocalipsis {
+      if(  !Apocalipsis.instance  ) {
+        Apocalipsis.instance = new Apocalipsis('Soy Apocalipsis')
+      }
+
+      return Apocalipsis.instance;
+    };
+
+    changeName( newName:string): void {
+      this.name = newName;
+    };
+  };
 
 
+  const apocalipsis1 = Apocalipsis.callApocalipsis();
+  const apocalipsis2 = Apocalipsis.callApocalipsis();
+  const apocalipsis3 = Apocalipsis.callApocalipsis();
+
+  apocalipsis1.changeName('Xavier')
+  // const apocalipsis1 = new Apocalipsis('Soy apocalipsis1...el unico');
+  // const apocalipsis2 = new Apocalipsis('Soy apocalipsis2...el unico');
+  // const apocalipsis3 = new Apocalipsis('Soy apocalipsis3...el unico');
+  console.log(apocalipsis1, apocalipsis2, apocalipsis3);
+  
 
   
 })();
