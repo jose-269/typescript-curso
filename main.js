1f8634f907f331e3638f5a79572630f79b096905
jose-269 <jose.luis@destacados.cl>
2022-02-02 17:28:56 -0300

    primeras clases de namespaces

--- main.js
+++ main.js
@@ -1,43 +1,15 @@
 "use strict";
-// Crear interfaces
-const conducirBatimovil = (auto) => {
-    auto.encender = true;
-    auto.velocidadMaxima = 100;
-    auto.acelear();
-};
-const batimovil = {
-    encender: false,
-    velocidadMaxima: 0,
-    acelear() {
-        console.log("...... gogogo!!!");
-    }
-};
-const guason = {
-    reir: true,
-    comer: true,
-    llorar: false
-};
-const reir = (guason) => {
-    if (guason.reir) {
-        console.log("JAJAJAJA");
-    }
-};
-const ciudadGotica = (ciudadanos) => {
-    return ciudadanos.length;
-};
-class Persona {
-    constructor(nombre, edad, sexo, estadoCivil) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.sexo = sexo;
-        this.estadoCivil = estadoCivil;
-    }
-    ;
-    imprimirBio() {
-        console.log(`${this.nombre} ${this.edad} ${this.sexo} ${this.estadoCivil}`);
-    }
-}
+var Validation;
+(function (Validation) {
+    Validation.validateText = (text) => {
+        return (text.length > 3) ? true : false;
+    };
+    Validation.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf()))
+            ? false
+            : true;
+    };
+})(Validation || (Validation = {}));
 ;
-const JoseM = new Persona('Jose', 37, 'Masculino', 'Soltero');
-JoseM.imprimirBio();
+console.log(Validation.validateText('Jose'));
 //# sourceMappingURL=main.js.map
